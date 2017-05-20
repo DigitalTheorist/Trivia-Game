68e965306032c03189aaaa6f05275a28317d7a6b
jcostopulos <costopulos@gmail.com>
2017-05-19 18:56:36 -0700

    basic css styling complete

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+
+//PSEUDOCODE GOES HERE
+
+//TODO: MOVIE QUOTE TRIVIA!
+
+//single start button starts game.
+
+//display single question
+//--TODO: math.randomized for loop iterate through "question array".
+//--TODO: Question is displayed with possible answers.
+//--TODO: timer starts for that question only.
+
+var questionAndAnswers = { //declare object. 
+
+  questions: [
+
+  'What was the black knights statement to King Aurther in Monty Pythons movie "Quest for the Holy Grail"?',
+
+  'Who said "Game over man! Game over!" and in what movie?',
+
+  'In what city did the first Terminator take place?'],
+
+  answers: [
+    '"None shall pass."',
+    'Hudson, Aliens',
+    'Los Angeles']
+};
+
+//player chooses questions
+//--TODO: each question is highlighted as cursor hovers.
+//--TODO:
+
+
+
+
+
+$(document).ready(function(){
+
+
+
+
+
+
+}); //Document ready endtag.
