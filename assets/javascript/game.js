8da8a7ddd49191645dc6f632fb72b8d8f60aa65c
jcostopulos <costopulos@gmail.com>
2017-05-22 21:30:34 -0700

    question buttons retriving object values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,18 +6,16 @@
 $('#startButton').click(timerBlock);
 
     function timerBlock () {
-    var count = 5;
+    var count = 6;
     var counter = setInterval(timer, 1000)
 
-    trivaBucket.question1.operator1();
+    trivaBucket.answer1.operator1();
 
     function timer(){
     count -= 1;
     console.log("count is now = " + count)
 
     $("#timerArea").html(count + " seconds left to answer!");
-
-    if () //TODO: If answer1 is clicked before timer ends move to "correct" screen
 
     if (count <= 0){
     clearInterval(counter);
@@ -26,6 +24,9 @@ $('#startButton').click(timerBlock);
     }
   }
 }
+
+
+
     //This 'this' function pushes data to Q&A divs.
 //------------------------------------------
 var objectFunction = function () {
@@ -38,7 +39,7 @@ var objectFunction = function () {
 
 var trivaBucket = {
 
-  question1: {
+  answer1: {
     name: '#answer1',
     question: 'what color is the sky?',
     trueAnswer: 'blue',
@@ -48,7 +49,7 @@ var trivaBucket = {
     operator1: objectFunction
   },
 
-  question2: {
+  answer2: {
     name: '#answer2',
     question: 'San Francisco is in what State?',
     trueAnswer: 'California',
@@ -58,7 +59,7 @@ var trivaBucket = {
     operator2: objectFunction
   },
 
-  question3: {
+  answer3: {
     name: '#answer3',
     question: "In what city is Apple's Headquarters?",
     trueAnswer: 'Cupertino',
@@ -69,7 +70,7 @@ var trivaBucket = {
 
   },
 
-  question4: {
+  answer4: {
     name: '#answer4',
     question: 'Is water wet?',
     trueAnswer: 'Yes',
@@ -82,6 +83,30 @@ var trivaBucket = {
 };
 //------------------------------------------
     //end object
+
+
+
+    //attributes the id of the block to
+    //playGame (clickIdentifier); //pass the attributes of "this" to the function "playGame"
+//------------------------------------------
+  $(".answerBlock").on("click", function(){
+
+  var clickIdentifier = $(this).attr('id');
+
+  console.log("the answer button you clicked on returns " + clickIdentifier); //logs answer1 ect...
+  playGame(clickIdentifier)
+});
+//------------------------------------------
+    // if () //TODO: If answer1 is clicked before timer ends move to "correct" screen
+//------------------------------------------
+  function playGame (clickIdentifier){
+  var chooseAnswer = trivaBucket[clickIdentifier]
+
+  console.log("Which is... " + chooseAnswer.trueAnswer);
+};
+//------------------------------------------
+
+
 
 }); //Document ready endtag.
 
