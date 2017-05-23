994ef0c72b6f1acb5723bf7178bdbe4587f2ba2c
jcostopulos <costopulos@gmail.com>
2017-05-22 20:15:03 -0700

    start timer functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,98 @@
+  $(document).ready(function(){
 
-//PSEUDOCODE GOES HERE
+          //GAME/TIMER START BUTTON
+//------------------------------------------
 
-//TODO: MOVIE QUOTE TRIVIA!
+$('#startButton').click(timerBlock);
 
-//single start button starts game.
+    function timerBlock () {
+    var count = 5;
+    var counter = setInterval(timer, 1000)
 
-//display single question
-//--TODO: math.randomized for loop iterate through "question array".
-//--TODO: Question is displayed with possible answers.
-//--TODO: timer starts for that question only.
+    trivaBucket.question1.operator1();
 
-var questionAndAnswers = { //declare object. 
+    function timer(){
+    count -= 1;
+    console.log("count is now = " + count)
 
-  questions: [
+    $("#timerArea").html(count + " seconds left to answer!");
 
-  'What was the black knights statement to King Aurther in Monty Pythons movie "Quest for the Holy Grail"?',
+    if () //TODO: If answer1 is clicked before timer ends move to "correct" screen
 
-  'Who said "Game over man! Game over!" and in what movie?',
+    if (count <= 0){
+    clearInterval(counter);
+    console.log("Ah! You didn't answer in time!")
 
-  'In what city did the first Terminator take place?'],
-
-  answers: [
-    '"None shall pass."',
-    'Hudson, Aliens',
-    'Los Angeles']
+    }
+  }
+}
+    //This 'this' function pushes data to Q&A divs.
+//------------------------------------------
+var objectFunction = function () {
+  $(".questionArea").html(this.question);
+  $(".answer1").html(this.trueAnswer);
+  $(".answer2").html(this.falseOne);
+  $(".answer3").html(this.falseTwo);
+  $(".answer4").html(this.falseThree);
 };
 
-//player chooses questions
-//--TODO: each question is highlighted as cursor hovers.
-//--TODO:
+var trivaBucket = {
 
+  question1: {
+    name: '#answer1',
+    question: 'what color is the sky?',
+    trueAnswer: 'blue',
+    falseOne: 'red',
+    falseTwo: 'green',
+    falseThree: 'violet',
+    operator1: objectFunction
+  },
 
+  question2: {
+    name: '#answer2',
+    question: 'San Francisco is in what State?',
+    trueAnswer: 'California',
+    falseOne: 'Texas',
+    falseTwo: 'North Dakota',
+    falseThree: 'Florida',
+    operator2: objectFunction
+  },
 
+  question3: {
+    name: '#answer3',
+    question: "In what city is Apple's Headquarters?",
+    trueAnswer: 'Cupertino',
+    falseOne: 'San Francisco',
+    falseTwo: 'Atlanta',
+    falseThree: 'Boston',
+    operator3: objectFunction
 
+  },
 
-$(document).ready(function(){
+  question4: {
+    name: '#answer4',
+    question: 'Is water wet?',
+    trueAnswer: 'Yes',
+    falseOne: 'no',
+    falseTwo: 'what?',
+    falseThree: 'I need some coffee.',
+    operator4: objectFunction
+  }
 
-
-
-
-
+};
+//------------------------------------------
+    //end object
 
 }); //Document ready endtag.
+
+
+
+//TODO: display correct for 5 seconds
+
+//TODO: move to next questions
+
+//TODO: otherwise move to "nope" display
+
+//TODO: display incorrect for 5 seconds
+
+//TODO: move to next question
